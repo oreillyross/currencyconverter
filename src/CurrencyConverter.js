3ad6b0ecfda7e7cbb5de6d8142b63208e0a93201
Ross O'Reilly <oreillyross@gmail.com>
2021-06-09 17:47:27 +0200

    handle inputs

--- src/CurrencyConverter.js
+++ src/CurrencyConverter.js
@@ -1,24 +1,47 @@
 import React from "react";
 
+const upper = (str) => str.toUpperCase();
+
 class CurrencyConverter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currSource: "" };
+  }
+
+  handleChange = (e) => {
+    const value = e.target.value;
+    this.setState({ ...this.state, [e.target.name]: value });
+  };
+  validate = (e) => {
+    console.log(e.target.value);
+  };
+
   render() {
     return (
       <>
         <h3>Currency Converter</h3>
         <section>
           <form>
-              <label>Source currency</label>
-            <input type="text" name="currSource" />
-              <label>Destination currency</label>
-            <input type="text" name="currDest" />
-              <label> Currency date</label>
-            <input type="date" name="currDate" />
-          <section className="buttons">
-
-
-            <button>Submit</button>
-            <button>Reset</button>
-          </section>
+            <label>Source currency</label>
+            <input
+              value={this.state.currSource}
+              type="text"
+              name="currSource"
+              onChange={this.handleChange}
+            />
+            <label>Destination currency</label>
+            <input
+              type="text"
+              name="currDest"
+              onKeyDown={this.validate}
+              onChange={this.handleChange}
+            />
+            <label> Currency date</label>
+            <input type="date" name="currDate" onChange={this.handleChange} />
+            <section className="buttons">
+              <button>Submit</button>
+              <button>Reset</button>
+            </section>
           </form>
           <div>The Exchange rate is ???</div>
         </section>
